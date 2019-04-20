42e06c72eb34b58f949c5ad6e6e5b23f6005d519
andrewdnolan <andrew.d.nolan@maine.edu>
2019-04-20 00:12:30 -0400

    better giff

--- geology/quake_giff.py
+++ geology/quake_giff.py
@@ -2,28 +2,59 @@ import pandas as pd
 import geopandas as gpd
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-import cartopy.io.img_tiles as cimgt
 from shapely.geometry import Point
+import cartopy.io.img_tiles as cimgt
+
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
 
+extent = [-107.5, -89.85, 30.6, 41.15]
+
+request = cimgt.OSM()
+
+# fig = plt.figure(figsize=(8, 5))
+# ax = plt.axes(projection=ccrs.Mercator())#request.crs)
+#
+#
+# ax.set_extent(extent)
+#
+# ax.add_image(request, 6, interpolation='bilinear')
+#
 quakes_df = pd.read_csv('../data/okQuakes.csv', parse_dates=['time'])
 crs = {'init' : 'epsg:4326'}
 geometry = [Point(Lat, Lon) for Lat, Lon in zip(quakes_df['longitude'], quakes_df['latitude'])]
 
 quakes_shp = gpd.GeoDataFrame(quakes_df, crs = crs, geometry = geometry)
 
+# plt.close()
+# fig.savefig('./test2.png', bbox_inches='tight')
+# # extent = [-107.5, -89.85, 30.6, 41.15]
+# #
+#
+# fig = plt.figure(figsize=(8, 5))
+# ax = plt.axes(projection=request.crs)#request.crs)
+#
+# ax.set_extent(extent)
+# ax.add_image(request, 6, interpolation='bilinear')
+#
+# ax.set_xlim(-107.5, -89.85)
+# ax.set_ylim(30.6, 41.15)
+#
 
 for year in range(1973, 2017):
+    fig = plt.figure(figsize=(8, 5))
+    ax = plt.axes(projection=request.crs)#request.crs)
 
-    fig, ax = plt.subplots(1)
-    ax.set_xlim(-107.54225322580646, -89.849746774193534)
-    ax.set_ylim(30.587871428571429, 41.148128571428572)
+    ax.set_extent(extent)
+    ax.add_image(request, 6, interpolation='bilinear')
+
+    # ax.set_xlim(-107.5, -89.85)
+    # ax.set_ylim(30.6, 41.15)
+
     mag = quakes_shp.loc[quakes_shp['time'].dt.year == year, 'mag']
-    quakes_shp[quakes_shp['time'].dt.year == year].plot(ax = ax, markersize = mag * 10)
+    quakes_shp[quakes_shp['time'].dt.year == year].plot(ax = ax, markersize = mag * 10, transform = ccrs.Geodetic())
 
     ax.set_title(year)
     plt.tight_layout()
-    fig.savefig('../figs/giff/Ok_quakes_{}.png'.format(year), )
     plt.close()
-
-plt.show()
+    fig.savefig('../figs/giff/Ok_quakes_{}.png'.format(year), dpi = 400, bbox_inches='tight')
